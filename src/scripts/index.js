5e2f9946999503c659594086e87dc7f7b80effaa
dfaizaditya <dfaizaditya@gmail.com>
2023-02-20 15:21:56 +0700

    update 2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
