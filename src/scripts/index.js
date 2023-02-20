76f9d64f1578f0f1f5822cdcd39b811aa806140a
dfaizaditya <dfaizaditya@gmail.com>
2023-02-20 14:44:37 +0700

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,9 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
